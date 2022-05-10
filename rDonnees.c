b7438e7d703ed97652f82b37b72dc4e54fa0ef81
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-10 15:46:21 +0200

    V2 du code
    Il reste quelques modifications à faire

--- rDonnees.c
+++ rDonnees.c
@@ -1,31 +1,66 @@
 #include "maBiblio.h"
+#include "structures.h"
 
-///  Blinder numD lors de la demande de récupération de la donnée
-
-char* rDonnees(char liste[TL][NBC], int numD)//numD = numéro de la donnée. 1 = Nom sort etc
+///  Blinder num lors de la demande de rÃ©cupÃ©ration de la donnee
+t_sort rDonnees(char liste[TL][NBC], int numS, t_sort sort1)//Extrait sort par sort. numS = numero du sort
 {
-    int i = 0;
-    int j = 0;
-    int nbmpde = 0;
-    char* dExtraites = (char*)malloc(TL * sizeof(char));
-    for(i=0; i<TL; i++)
+    lFichier(liste);
+
+        ///LECTURE NOM DU SORT
+
+    memmove(sort1.nomS, liste[0], sizeof(char)*8);//Remplacer 0 par j
+    for(int i = 0; i<8; i++)
     {
-        for(j=0; j<NBC; j++)
-        {
-            if(liste[i][j] == '!')
-            {
-                nbmpde = nbmpde+1;
-            }
-            if(nbmpde == numD)
-            {
-                dExtraites[j] = liste[TL][j+1];
-            }
-        }
+        printf("%c", sort1.nomS[i]);
     }
-    for(i=0; i<TL; i++)
-    {
-        printf("%c\n", dExtraites[i]);
-    }
-    return dExtraites;
-    free(dExtraites);
+    printf("\n");
+
+    ///LECTURE PORTEE MINIMALE
+
+    sort1.porteeMin = liste[0][9]-DTA;
+    printf("%d", sort1.porteeMin);
+    printf("\n");
+
+    ///LECTURE PORTEE MAXIMALE
+
+    sort1.porteeMax = liste[0][11]-DTA;
+    printf("%d", sort1.porteeMax);
+    printf("\n");
+
+    ///LECTURE COUT PA
+
+    sort1.paS = liste[0][13]-DTA;
+    printf("%d", sort1.paS);
+    printf("\n");
+
+    ///LECTURE EFFET 1
+
+    sort1.effet1 = liste[0][15]-DTA;
+    printf("%d", sort1.effet1);
+    printf("\n");
+
+    ///LECTURE EFFET 2
+
+    sort1.effet2 = liste[0][17]-DTA;
+    printf("%d", sort1.effet2);
+    printf("\n");
+
+    ///LECTURE TYPE SORT
+
+    sort1.type = liste[0][19]-DTA;
+    printf("%d", sort1.type);
+    printf("\n");
+
+    ///LECTURE PROBA EFFET 1
+
+    sort1.proba1 = liste[0][21]-DTA;
+    printf("%d", sort1.proba1);
+    printf("\n");
+
+    ///LECTURE PROBA EFFET 2
+
+    sort1.proba2 = liste[0][23]-DTA;
+    printf("%d", sort1.proba2);
+    printf("\n");
+    return sort1;
 }
