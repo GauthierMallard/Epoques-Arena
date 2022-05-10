118f8c270f1602d862e37b113b887b31337d334c
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-10 15:59:08 +0200

    Update maBiblio.h

--- maBiblio.h
+++ maBiblio.h
@@ -1,8 +1,10 @@
 #ifndef MABIBLIO_H_INCLUDED
 #define MABIBLIO_H_INCLUDED
+#include "structures.h"
 
-#define TL 4
-#define NBC 50
+#define TL 4 //Taille de la liste de sorts
+#define NBC 50 //Nombre de caracteres sur une ligne du fichier
+#define DTA 48 //Decalage table ASCII pour les entiers.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +14,11 @@
 
 
 ///Prototypes des fonctions
-int lFichier(char liste[TL][NBC]);
-void affListe(char liste[TL][NBC]);
-char* rDonnees(char liste[TL][NBC], int numD);
+int lFichier(char liste[TL][NBC]);//Lecture du fichier ou sont contenus les sorts
+void affListe(char liste[TL][NBC]);//Affiche la liste dans laquelle on recupere le fichier
+t_sort rDonnees(char liste[TL][NBC], int numS, t_sort sort1);//Copie des donnees dans une structure de type t_sort
+
+//Je vais prototyper le ss programme permettant de creer une classe de sorts.
+//Il faudra recuperer ce ss programme et l'associer a un personnage par la suite -> Pour celui qui s'en occupe.
 
 #endif // MABIBLIO_H_INCLUDED
