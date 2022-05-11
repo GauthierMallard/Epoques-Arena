a7b296c4fa37df6a1366fee16beddd06e9f3ba5d
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-11 17:24:01 +0200

    Update main
    Fonctionne désormais uniquement avec un appel de la fonction cJoueur

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include "maBiblio.h"
-
 int main()
 {
-    t_classes classe1;
-    cClasses(classe1);
-    cClasses1(classe1);
-    cClasses2(classe1);
+    t_joueur joueur;//Creation de joueur, structure de type t_joueur
+    joueur = cJoueur(joueur);//On affecte a joueur les donnees recup par la fonction
+    testRecupJoueur(joueur);//Procedure permettant d'afficher les donnees stockees dans t_joueur et de verifier le bon fonctionnement de cJoueur
     return 0;
 }
