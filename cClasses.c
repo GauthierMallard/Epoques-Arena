f490980ecdfbdffe6ad7333c97c3ecb1905b1acb
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-11 17:14:20 +0200

    Update cClasses

--- cClasses.c
+++ cClasses.c
@@ -5,14 +5,12 @@ t_classes cClasses(t_classes classe1)
     int numS = 0;
     char liste[TL][NBC];
     t_sort sort;
-    printf("ENTRER NOM :\n");
-    gets(classe1.nomC);
     lFichier(liste);
     for(numS=0; numS<4; numS++)
     {
 
         classe1.sorts[numS] = rDonnees(liste,numS,sort);
-        for(int i = 0; i<16; i++)
+     /*   for(int i = 0; i<16; i++)
         {
             printf("%c", classe1.sorts[numS].nomS[i]);
         }
@@ -56,7 +54,8 @@ t_classes cClasses(t_classes classe1)
         ///LECTURE PROBA EFFET 2
 
         printf("%d", classe1.sorts[numS].proba2);
-        printf("\n");
+        printf("\n");*/
+        ///LE CODE EN COMMENTAIRE AU DESSUS SERT A VERIFIER LE FONCTIONNEMENT DU SS PROGRAMME
     }
     return classe1;
 }
