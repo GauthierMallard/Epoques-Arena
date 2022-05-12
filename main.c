53bc0d9df54481054a06411056602bf16d56f259
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-12 18:44:38 +0200

    Update main
    Ajout fonction nbmJoueurs

--- main.c
+++ main.c
@@ -3,6 +3,9 @@ int main()
 {
     t_joueur joueur;//Creation de joueur, structure de type t_joueur
     joueur = cJoueur(joueur);//On affecte a joueur les donnees recup par la fonction
+    ///LA LIGNE PRECEDENTE EST VOUEE A DISPARAITRE POUR ETRE REMPLACEE PAR LA SUIVANTE
+    //nbmJoueurs();
+    ///      /!\       NE PAS TESTER nbmJoueurs() AVANT D'AVOIR PLACE UN FREE
     testRecupJoueur(joueur);//Procedure permettant d'afficher les donnees stockees dans t_joueur et de verifier le bon fonctionnement de cJoueur
     return 0;
 }
