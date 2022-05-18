489e834926b4f6e4dd18ac24402d288ecfc2894a
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-18 17:42:01 +0200

    Update maBiblio.h
    -Ajout des define lignes et colonnes

--- maBiblio.h
+++ maBiblio.h
@@ -1,10 +1,15 @@
 #ifndef MABIBLIO_H_INCLUDED
 #define MABIBLIO_H_INCLUDED
-#include "structures.h"
+
+///Les differents define
 
 #define TL 4 //Taille de la liste de sorts
 #define NBC 50 //Nombre de caracteres sur une ligne du fichier
 #define DTA 48 //Decalage table ASCII pour les entiers.
+#define LIGNES 20 //Nombre de lignes dans la matrice carte
+#define COLONNES 20 //Nombre de colonnes dans la matrice carte
+
+///Les differents include
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +19,8 @@
 #include <allegro.h>
 #include "structures.h"
 
-
-
 ///Prototypes des fonctions
+
 int lFichier(char liste[TL][NBC]);//Lecture du fichier ou sont contenus les sorts de la premiere classe
 int lFichier1(char liste[TL][NBC]);//Lecture du fichier ou sont contenus les sorts de la 2eme classe
 int lFichier2(char liste[TL][NBC]);//Lecture du fichier ou sont contenus les sorts de la 3eme classe
@@ -27,7 +31,6 @@ t_classes cClasses(t_classes classe1);//Fonction appellee lors de la creation d'
 t_classes cClasses1(t_classes classe1);//Identique a cClasses, c'est juste pas le meme fichier ouvert
 t_classes cClasses2(t_classes classe1);//Identique a cClasses, c'est juste pas le meme fichier ouvert
 t_classes cClasses3(t_classes classe1);//Identique a cClasses, c'est juste pas le meme fichier ouvert
-
 t_joueur cJoueur(t_joueur joueur,int choix);//Fonction permettant le remplissage d'une structure de type t_joueur. Renvoie une structure de type t_joueur
 t_joueur* nbmJoueurs();//Fonction permettant l'allocation d'un tableau dynamique en fonction du nbm de joueurs. Renvoie le tableau par adresse
 void testRecupJoueur(t_joueur joueur);//Procédure permettant d'afficher les donnees stockees pour verifier bonne recuperation
@@ -38,8 +41,6 @@ void lancermenu(BITMAP* menu,BITMAP* curseur,int* avjeu);//affiche le menu et ge
 void nombrejoueurs(BITMAP* fond,BITMAP* curseur,int* avjeu,int* nbjoueur);//permet de choisir le nombre de joueurs avant le debut de la partie
 void credits(BITMAP* fond,BITMAP* curseur,int* avjeu);// affiche les credits
 void choixclasse(BITMAP* fond,BITMAP* curseur,int* avjeu,int* nbjoueur,t_joueur* joueurs[]);//permet au joueur de choisir sa classe
-
-//procedure de redirection
 void hubmenus(BITMAP* menu,BITMAP* curseur,BITMAP* cjoueur,BITMAP *credit,int* avjeu,int* nbjoueur);//redirige le programmes vers l'action effectue par le joueur
 
 
