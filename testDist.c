c5405b47a91979fb598c51763a627117fab228d8
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-19 12:58:54 +0200

    Update testDist.c

--- testDist.c
+++ testDist.c
@@ -2,13 +2,13 @@
 
 int testDist(t_joueur* lanceur, int porteeMin, int porteeMax, int coordx, int coordy)
 {
-    if(abs(lanceur.coordJ.lignes - coordx)>porteeMin)
+    if(abs(lanceur->coordJ.lignes - coordx)>porteeMin)
     {
-        if(abs(lanceur.coordJ.lignes - coordx)<porteeMax)
+        if(abs(lanceur->coordJ.lignes - coordx)<porteeMax)
         {
-            if(abs(lanceur.coordJ.colonnes - coordy)>porteeMin)
+            if(abs(lanceur->coordJ.colonnes - coordy)>porteeMin)
             {
-                if(abs(lanceur.coordJ.colonnes - coordy)<porteeMax)
+                if(abs(lanceur->coordJ.colonnes - coordy)<porteeMax)
                 {
                     return 1;
                 }
