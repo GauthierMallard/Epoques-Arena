d1dfbe9a9eaec836bf5a22aa46f01c7a6d71ee2e
Hugo MAZERAT <96187120+HugoMazerat@users.noreply.github.com>
2022-05-21 14:01:56 +0200

    Update chemin.c

--- chemin.c
+++ chemin.c
@@ -2,13 +2,8 @@
 
 void chemin(BITMAP* decor,BITMAP* page, BITMAP* persocourt,BITMAP* perso0w,BITMAP* perso1w,BITMAP* perso2w,BITMAP* perso3w,BITMAP* coeur,BITMAP* fondlave,BITMAP* epee,BITMAP* chaussure,BITMAP* joueurSuivant,char nomfichier[256], t_joueur* joueurs, int* nbjoueur, int jqj, t_cases dest)
 {
-    int numPersoCourt = 1;
-    if(abs(joueurs[jqj].coordJ.lignes - dest.lignes)<32)
+    if(abs(joueurs[jqj].coordJ.lignes - dest.lignes)<64 || abs(joueurs[jqj].coordJ.colonnes - dest.colonnes)<64)
     {
-        animationPersonnageCourtC(decor, page, persocourt, perso0w, perso1w, perso2w, perso3w, coeur, fondlave, epee, chaussure, joueurSuivant, nomfichier[256], joueurs, nbjoueur, jqj, dest);
-    }
-    if(abs(joueurs[jqj].coordJ.colonnes - dest.colonnes)<32)
-    {
-        animationPersonnageCourtL(decor, page, persocourt, perso0w, perso1w, perso2w, perso3w, coeur, fondlave, epee, chaussure, joueurSuivant, nomfichier[256], joueurs, nbjoueur, jqj, dest);
+        animationPersonnageCourt(decor, page, persocourt, perso0w, perso1w, perso2w, perso3w, coeur, fondlave, epee, chaussure, joueurSuivant, nomfichier, joueurs, nbjoueur, jqj, dest);
     }
 }
